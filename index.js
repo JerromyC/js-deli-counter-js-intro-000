ef5b4f68d13db4e526ce9ce0f32b1389923753ae
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-29 03:06:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function takeANumber(line, name) {
 
   return `Welcome, ${name}. You are number ` + line.length + ` in line.`
 }
+
+function nowServing() {
+  if(!line.length) {
+      console.log(`There is nobody waiting to be served!`);
+  }
+
+}
